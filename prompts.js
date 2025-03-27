bd1dd5c0f6c203e38de5dda469e5f6d022da112b
Dat <38579777+stevedat@users.noreply.github.com>
2025-03-27 22:52:52 +0700

    Create prompts.js

---
+++ prompts.js
@@ -0,0 +1,47 @@
+const prompts = [
+    ["hi", "hiii", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    ["your name please", "your name", "may i know your name", "what is your name", "what call yourself"],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no", "not sure", "maybe", "no thanks"],
+    [""],
+    ["haha", "ha", "lol", "hehe", "funny", "joke"]
+];
+
+const replies = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"],
+    ["I'm doing great, how about you?", "I'm fine, thanks!", "Pretty well, what about you?"],
+    ["Just chatting!", "Thinking...", "Nothing much, you?"],
+    ["I am infinite."],
+    ["I'm a chatbot!", "I am a bot. What are you?"],
+    ["A developer created me.", "I was made by JavaScript magic."],
+    ["I have no name, just call me Bot!", "I'm just a chatbot."],
+    ["I love you too!", "Aww, thanks!"],
+    ["That's great!", "Nice to hear that!"],
+    ["Oh no, why?", "I'm here to chat, tell me more."],
+    ["Sure! Once upon a time...", "Here's a joke: Why did the chicken cross the road?"],
+    ["Hmm?", "Okay!", "Sounds good!"],
+    ["Goodbye!", "See you later!", "Take care!"],
+    ["How about some sushi?", "Pizza is always a good choice!", "Try a healthy meal!"],
+    ["Bro!"],
+    ["Good question!"],
+    ["That's okay!", "I understand.", "Let's talk about something else!"],
+    ["Say something!"],
+    ["Haha!", "Nice one!", "Good joke!"]
+];
+
+const alternative = ["Hmm...", "Tell me more!", "I don't understand...", "Can you rephrase?"];
+
+const coronavirus = ["Stay home!", "Wear a mask!", "Be careful!", "Stay safe!"];
